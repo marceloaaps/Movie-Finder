0b2a54584f592d7a48d2a423d97ed6f4c2cdc45f
marceloaaps <marceloalexandreaps@gmail.com>
2024-06-05 03:44:15 -0300

    Criação MVC.

---
+++ projectSrc/View/js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('buttonSM').addEventListener('click', function() {
+    window.location.href = 'detalhamento.html'; 
+});
+
+document.getElementById('quitButton').addEventListener('click', function() {
+    window.location.href = 'login.html'; 
+});
--- projectSrc/web/js/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('buttonSM').addEventListener('click', function() {
-    window.location.href = 'detalhamento.html'; 
-});
-
-document.getElementById('quitButton').addEventListener('click', function() {
-    window.location.href = 'login.html'; 
-});
